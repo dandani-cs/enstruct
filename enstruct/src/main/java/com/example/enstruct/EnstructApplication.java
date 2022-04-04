e8dd1937dc60bb5e50d8b83e38e83cd5b41e21ca
Dani <dani.cstech@gmail.com>
2022-04-04 16:28:21 +0800

    first commit

---
+++ enstruct/src/main/java/com/example/enstruct/EnstructApplication.java
@@ -0,0 +1,13 @@
+package com.example.enstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnstructApplication.class, args);
+	}
+
+}
---
+++ enstruct/src/test/java/com/example/enstruct/EnstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
